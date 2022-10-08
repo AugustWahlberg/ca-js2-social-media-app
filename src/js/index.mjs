2d9c64cd94ab5c818711e878ef0b8b8b8789f062
AugustWahlberg <aug.wahlberg@gmail.com>
2022-10-08 20:27:11 +0200

    Added login functionality

--- src/js/index.mjs
+++ src/js/index.mjs
@@ -1,4 +1,4 @@
-const loginbtn = document.querySelector("#loginbtn")
+/*const loginbtn = document.querySelector("#loginbtn")
 loginbtn.addEventListener("click", event =>{
   event.preventDefault()
   location.href = "/profile/login";
@@ -9,3 +9,40 @@ signInButtun.addEventListener("click", event =>{
   event.preventDefault()
   location.href = "/profile/register";
 })
+*/
+
+
+import { setRegisterFormListener } from "./handlers/register.mjs";
+import { setLoginFormListener } from "./handlers/login.mjs";
+//import * as templates from "./templates/index.mjs"
+//import * as postMethods from "./api/posts/index.mjs"
+//import { renderPostTemplates } from "./template/post.mjs";
+
+
+const path = location.pathname;
+
+if (path === '/profile/login/') {
+  setLoginFormListener()
+} else if (path === '/profile/register/') {
+  setRegisterFormListener()
+}
+
+/*
+post.createPost()
+post.updatePost()
+post.removePost()
+post.getPost() 
+post.getPosts().then(console.log)
+*/
+
+/*
+async function testTemplate (){
+  const posts = await postMethods.getPosts();
+  const post = posts[1];
+  const container = document.querySelector("#post")
+  renderPostTemplates(post, container)
+}
+
+testTemplate()
+
+*/
