bfeb32f27f9b528a30345c52e475842ab6dc328a
AugustWahlberg <aug.wahlberg@gmail.com>
2022-10-09 03:25:54 +0200

    Added extra form validation

--- src/js/handlers/register.mjs
+++ src/js/handlers/register.mjs
@@ -12,9 +12,31 @@ if (form) {
      const form = event.target;
      const formData = new FormData(form);
      const profile = Object.fromEntries(formData.entries())
-  
-     // Send it to the API
+     const inputPasswordError2 = document.getElementById("password-error-2");
+     const termsError= document.getElementById("terms-error");
+     const terms = document.getElementById('terms')
+
+      if (profile.password != profile.passwordRepeat ) {
+        inputPasswordError2.style.display = "block";
+      }
+    
+      else {
+        inputPasswordError2.style.display = "none";
+      }
+
+      if(terms.checked == true){
+        termsError.style.display = "none"
+      }
+      
+      else {
+        console.log("not checked")
+        termsError.style.display = "block"
+      }
+
+      if (profile.password === profile.passwordRepeat && terms.checked == true){
+        // Send it to the API
      register (profile)
+      }    
   })
 }
 }
