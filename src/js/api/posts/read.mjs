acf5cad78baabe189df82ec8028c558b1fedd460
AugustWahlberg <aug.wahlberg@gmail.com>
2022-10-12 16:39:02 +0200

    Added getPosts function

--- src/js/api/posts/read.mjs
+++ src/js/api/posts/read.mjs
@@ -1,11 +1,12 @@
 import { API_SOCIAL_URL } from "../constants.mjs";
 import { authFetch } from "../authFetch.mjs";
 
-const action = "/posts";
+const posts = "/posts";
+const allPostData = "/?_author=true&_comments=true&_reactions=true"
 
 export async function getPosts() {
-  const updatePostURL = `${API_SOCIAL_URL}${action}`
-  const response = await authFetch (updatePostURL)
+  const allPostsURL = `${API_SOCIAL_URL}${posts}${allPostData}`
+  const response = await authFetch (allPostsURL)
   return await response.json();
 }
 
@@ -14,7 +15,19 @@ export async function getPost(id) {
   if (!id){
     throw new Error("Get requires a postID");
   }
-  const getPostURL = `${API_SOCIAL_URL}${action}/${id}`;
+  const getPostURL = `${API_SOCIAL_URL}${posts}/${id}`;
   const response = await authFetch (getPostURL)
   return await response.json();
 }
+
+const profiles = "/profiles/";
+const userPosts= "?_posts=true&_following=true&_followers=true"
+
+
+ export async function getPostsUser() {
+   const profileData = JSON.parse(localStorage.getItem('profile'));
+   const name = profileData.name;
+   const usersPostURL = `${API_SOCIAL_URL}${profiles}${name}${userPosts}`
+   const response = await authFetch (usersPostURL)
+   return await response.json();
+ }
