1f6c442f93202bc34669123bcf194ff2a2883454
AugustWahlberg <aug.wahlberg@gmail.com>
2022-10-15 15:25:05 +0200

    Fixed search func

--- src/js/api/posts/search.js
+++ src/js/api/posts/search.js
@@ -1,18 +1,19 @@
-import { getPosts } from "./read.mjs";
+import * as templates from "../../templates/index.mjs"
 
 export function searchPosts(posts) {
     const search = document.querySelector("#search");
-
+    console.log(search.value);
     search.onkeyup = function (event) {
-         //console.log(event);
+        // console.log(event,posts);
 
         const searchValue = event.target.value.trim().toLowerCase();
 
-        const filteredPosts = posts.filter(function (post) {
-            if (post.title.toLowerCase().startsWith(searchValue)) {
-                return true;
-            }
-        });
-
-        getPosts(filteredPosts);
+        const filteredPosts = posts.filter((post) =>{
+            return post.title.toLowerCase().startsWith(searchValue) || post.author.name.toLowerCase().startsWith(searchValue)
+        }
+           
+    );
+    const container = document.querySelector("#postfeed");
+    container.innerHTML = ""
+    templates.renderPostTemplates(filteredPosts, container)
     }};
