dbf043e368dabfd3500f23003602e497aead713c
AugustWahlberg <aug.wahlberg@gmail.com>
2022-10-15 15:24:26 +0200

    Added filter func

--- src/js/api/posts/filterPosts.mjs
+++ src/js/api/posts/filterPosts.mjs
@@ -1,5 +1,34 @@
-    const filterByUsername = document.querySelector("filterByUsername");
-    const filterByTitle = document.querySelector("filterByTitle");
-    const filterByTag = document.querySelector("filterByTag");
-    const filterByLatestPosts = document.querySelector("filterByLatestPosts");
-    const filterByOldest = document.querySelector("filterByOldest");
+const filterSelect = document.querySelector("#filterSelect");
+import * as templates from "../../templates/index.mjs"
+
+export function filterPosts(posts) {
+  filterSelect.addEventListener("change", (e) => {
+      
+    if (e.target.value === "newest") {
+      const filteredPosts = posts.filter((post) => {
+        return (
+          Math.abs(new Date(post.created).getDay() - new Date().getDay()) === 0
+        );
+      });
+      const sortedPosts = filteredPosts.sort((a,b)=> new Date(b.created) - new Date(a.created))
+      const container = document.querySelector("#postfeed");
+      container.innerHTML = ""
+      templates.renderPostTemplates(sortedPosts, container)
+    } else if(e.target.value === "oldest") {
+      const filteredPosts = posts.filter((post) => {
+        return (
+          Math.abs(new Date(post.created).getDay() - new Date().getDay()) > 0
+        );
+      });
+      const sortedPosts = filteredPosts.sort((a,b)=> new Date(b.created) - new Date(a.created))
+      const container = document.querySelector("#postfeed");
+      container.innerHTML = ""
+      templates.renderPostTemplates(sortedPosts, container)
+    }
+   else{
+        const container = document.querySelector("#postfeed");
+        container.innerHTML = ""
+        templates.renderPostTemplates(posts, container)
+      }
+  });
+}
