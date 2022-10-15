2e1d5f1288b4e0cbb8cca1cf1e95b47199394301
AugustWahlberg <aug.wahlberg@gmail.com>
2022-10-15 19:07:55 +0200

    Added removePost and updatePost func, not finished

---
+++ src/js/handlers/updatePost.mjs
@@ -0,0 +1,11 @@
+if (form) {
+  form.addEventListener("submit", (e) => {
+    e.preventDefault ()
+    const form = e.target;
+    const formData = new FormData (form);
+    const post = Object.fromEntries(formData.entries())
+    //Send it to the API
+    createPost(post);
+    alert ("New post was created")
+  })
+}
