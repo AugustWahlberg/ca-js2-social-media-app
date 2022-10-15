9d74ee84446ad96203c3f6e0657bcd16a65ee436
AugustWahlberg <aug.wahlberg@gmail.com>
2022-10-15 15:25:26 +0200

    Added new func to main mjs file

--- src/js/index.mjs
+++ src/js/index.mjs
@@ -5,9 +5,12 @@ import * as templates from "./templates/index.mjs"
 import * as postMethods from "./api/posts/index.mjs"
 import { getProfileBanner } from "./api/user/index.mjs";
 import { searchPosts } from "./api/posts/search.js";
+import { filterPosts } from "./api/posts/filterPosts.mjs";
 
 
 const path = location.pathname;
+
+// LOGIN and SIGNIN
 
 if (path === '/index.html') {
 const loginbtn = document.querySelector("#loginbtn")
@@ -30,12 +33,16 @@ if (path === '/profile/login/') {
   setRegisterFormListener()
 }
 
+
+// VIEW POSTS ON FEED
+
   async function viewPosts (){
     const posts = await postMethods.getPosts();
     //console.log(posts)
     const container = document.querySelector("#postfeed")
     templates.renderPostTemplates(posts, container)
     searchPosts(posts); 
+    filterPosts(posts)
   }
 
   
@@ -43,6 +50,8 @@ if (path === '/feed/') {
   viewPosts()
 }
 
+
+// VIEW BANNER
 
  async function viewBanner (){
   const profile = await getProfileBanner();
@@ -52,3 +61,25 @@ if (path === '/feed/') {
 }
 
 viewBanner()
+
+
+// VIEW USERS POSTS
+
+
+async function viewUserPosts (){
+  const user = await postMethods.getPostsUser();
+ console.log(user)
+  const container = document.querySelector("#profileBanner")
+ templates.renderPostUserTemplates(user.posts, container)
+}
+
+if (path === '/profile/') {
+  viewUserPosts()
+}
+
+
+if(path.contains("/post")){
+  viewUserPosts()
+const id = path.split("/");
+console.log(id);
+}
