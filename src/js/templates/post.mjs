d7a7491458b002bed9ba960e11736576ba045f78
AugustWahlberg <aug.wahlberg@gmail.com>
2022-10-15 15:24:03 +0200

    Added func to show single post

--- src/js/templates/post.mjs
+++ src/js/templates/post.mjs
@@ -1,4 +1,14 @@
-export function postTemplate(postData) {
+function displaySinglePost(postData) {
+  const banner = document.querySelector("#bannerID");
+  const createPost = document.querySelector("#createPost");
+  banner.style.display = "none";
+  createPost.style.display = "none";
+  const container = document.querySelector("#postfeed");
+  container.innerHTML = "";
+  container.append(postTemplate(postData, "single"))
+}
+
+export function postTemplate(postData, type) {
   const post = document.createElement("div");
   const id = postData.id;
   const avatar = postData.author.avatar;
@@ -9,13 +19,20 @@ export function postTemplate(postData) {
   const reactions = postData._count.reactions;
   const comments = postData._count.comments;
   const created = postData.created;
-  const examplePhoto = "https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460__340.png"
+  const examplePhoto =
+    "https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460__340.png";
 
   post.classList.add("post");
   post.setAttribute("id", id);
 
-  //console.log(postData.author)
-  post.innerHTML = ` <div class="row justify-content-center">
+  if (type === "multiple") {
+    post.addEventListener("click", (e) => {
+      displaySinglePost(postData);
+    });
+  }
+
+
+  post.innerHTML = `<div class="row justify-content-center">
   <div class="col-12 col-md-8 col-lg-6 col-xl-5 min-mx-post">
 <div class="card border-success mb-3 bg-torquise text-info">
   <div class="card-header border-success">
@@ -42,36 +59,43 @@ export function postTemplate(postData) {
 `;
   //const button = document.createElement("button")
   //post.append(button)
-  
+
   //button.addEventListener("click", () => console.log(postData))
   return post;
-  }
-  
-  export function renderPostTemplate(postData, parent){
-    parent.append(postTemplate(postData))
-  }
-  
-  export function renderPostTemplates(postDataList, parent) {
-    parent.append(...postDataList.map(postTemplate))
-    console.log()
-  }
+}
 
-  export function UserPostsTemplate(userPostData) {
-    const id = userPostData.id;
-    const avatar = userPostData.avatar;
-    const name = userPostData.name;
-    const body = userPostData.body;
-    const tags = userPostData.tags;
-    const reactions = userPostData._count.reactions;
-    const comments = userPostData._count.comments;
-    const created = userPostData.created;
-    const examplePhoto = "https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460__340.png"
-  
-    post.classList.add("post");
-    post.setAttribute("id", id);
-  
-    //console.log(postData.author)
-    post.innerHTML = ` <div class="row justify-content-center">
+export function renderPostTemplate(postData, parent) {
+  parent.append(postTemplate(postData, "multiple"));
+}
+
+export function renderPostTemplates(postDataList, parent) {
+  parent.append(...postDataList.map((item) => postTemplate(item, "multiple")));
+  console.log();
+}
+
+export function UserPostsTemplate(userPostData) {
+  console.log(userPostData);
+  const post = document.createElement("div");
+  const id = userPostData.id;
+  const avatar = userPostData.avatar;
+  const name = userPostData.owner;
+  const body = userPostData.body;
+  const tags = userPostData.tags;
+  const reactions = userPostData?._count?.reactions
+    ? userPostData?._count?.reaction
+    : 0;
+  const comments = userPostData?._count?.comments
+    ? userPostData?._count?.comments
+    : 0;
+  const created = userPostData.created;
+  const examplePhoto =
+    "https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460__340.png";
+
+  post.classList.add("post");
+  post.setAttribute("id", id);
+
+  //console.log(postData.author)
+  post.innerHTML = ` <div class="row justify-content-center">
     <div class="col-12 col-md-8 col-lg-6 col-xl-5 min-mx-post">
   <div class="card border-success mb-3 bg-torquise text-info">
     <div class="card-header border-success">
@@ -94,13 +118,12 @@ export function postTemplate(postData) {
   </div>
   </div>
   </div>
-  `
+  `;
+
+  return post;
 }
 
- export function renderPostUserTemplates(userPostDataList, parent) {
-     parent.append(...userPostDataList.map(UserPostsTemplate))
-     console.log()
-   }
-
-
- 
+export function renderPostUserTemplates(userPostDataList, parent) {
+  parent.append(...userPostDataList.map(UserPostsTemplate));
+  console.log();
+}
