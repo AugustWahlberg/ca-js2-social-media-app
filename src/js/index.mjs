20119c867ae409e7b10ba0c92974610c47656ee1
AugustWahlberg <aug.wahlberg@gmail.com>
2022-10-16 14:48:30 +0200

    Added edit and delete post func

--- src/js/index.mjs
+++ src/js/index.mjs
@@ -1,6 +1,10 @@
-import { setRegisterFormListener, setLoginFormListener, setCreatePostFormListener } from "./handlers/index.mjs";
-import * as templates from "./templates/index.mjs"
-import * as postMethods from "./api/posts/index.mjs"
+import {
+  setRegisterFormListener,
+  setLoginFormListener,
+  setCreatePostFormListener,
+} from "./handlers/index.mjs";
+import * as templates from "./templates/index.mjs";
+import * as postMethods from "./api/posts/index.mjs";
 import { getProfileBanner } from "./api/user/index.mjs";
 import { searchPosts } from "./api/posts/search.js";
 import { filterPosts } from "./api/posts/filterPosts.mjs";
@@ -9,69 +13,68 @@ const path = location.pathname;
 
 // LOGIN and SIGNIN
 
-if (path === '/index.html') {
-const loginbtn = document.querySelector("#loginbtn")
-loginbtn.addEventListener("click", event =>{
-  event.preventDefault()
-  location.href = "/profile/login";
-})
+if (path === "/index.html") {
+  const loginbtn = document.querySelector("#loginbtn");
+  loginbtn.addEventListener("click", (event) => {
+    event.preventDefault();
+    location.href = "/profile/login";
+  });
 
-const signInButton = document.querySelector("#signin-btn")
-signInButton.addEventListener("click", event =>{
-  event.preventDefault()
-  location.href = "/profile/register";
-})
+  const signInButton = document.querySelector("#signin-btn");
+  signInButton.addEventListener("click", (event) => {
+    event.preventDefault();
+    location.href = "/profile/register";
+  });
 }
 
-if (path === '/profile/login/') {
-  setLoginFormListener()
-} else if (path === '/profile/register/') {
-  setRegisterFormListener()
+if (path === "/profile/login/") {
+  setLoginFormListener();
+} else if (path === "/profile/register/") {
+  setRegisterFormListener();
 }
 
 // VIEW POSTS ON FEED
 
-  async function viewPosts (){
-    const posts = await postMethods.getPosts();
-    //console.log(posts)
-    const container = document.querySelector("#postfeed")
-    templates.renderPostTemplates(posts, container)
-    searchPosts(posts); 
-    filterPosts(posts)
-  }
+async function viewPosts() {
+  const posts = await postMethods.getPosts();
+  //console.log(posts)
+  const container = document.querySelector("#postfeed");
+  templates.renderPostTemplates(posts, container);
+  searchPosts(posts);
+  filterPosts(posts);
+}
 
-if (path === '/feed/') {
-  viewPosts()
+if (path === "/feed/") {
+  viewPosts();
 }
 
 // VIEW BANNER
 
- async function viewBanner (){
+async function viewBanner() {
   const profile = await getProfileBanner();
-  const feed = document.querySelector("#bannerID")
-  templates.renderProfileTemplate(profile, feed)
+  const feed = document.querySelector("#bannerID");
+  templates.renderProfileTemplate(profile, feed);
 }
 
-viewBanner()
+viewBanner();
 
 // VIEW USERS POSTS
 
-async function viewUserPosts (){
+async function viewUserPosts() {
   const user = await postMethods.getPostsUser();
- console.log(user)
-  const container = document.querySelector("#profileBanner")
- templates.renderPostUserTemplates(user.posts, container)
+  console.log(user);
+  const container = document.querySelector("#profileBanner");
+  templates.renderPostUserTemplates(user.posts, container);
 }
 
-if (path === '/profile/') {
-  viewUserPosts()
+if (path === "/profile/") {
+  const editPost = document.querySelector("#editPost");
+editPost.style.display="none"
+  viewUserPosts();
 }
 
- if(path.contains("/post")){
-   viewUserPosts()
- const id = path.split("/");
- console.log(id);
- }
+// CREATE POST
 
- // CREATE POST
-setCreatePostFormListener();
+if (path === "/feed/") {
+  setCreatePostFormListener();
+}
--- src/js/templates/profile.mjs
+++ src/js/templates/profile.mjs
@@ -1,66 +1,64 @@
-
-  /**
+/**
    * Function that create a banner template of for a user 
    * @param {object} profileData 
    */
-  export function bannerTemplate(profileData) {
-    
-    const name = profileData.name;
-    const numOnPosts = profileData._count.posts;
-    const followers = profileData.followers.length;
-    const following = profileData.following.length;
-    const avatar = profileData.avatar;
-    const examplePhoto = "https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460__340.png"
-    //console.log(avatar)
-    const banner = document.querySelector("#profileBanner")
-    
-    //console.log(postData.author)
-    banner.innerHTML = ` 
+ export function bannerTemplate(profileData) {
+  console.log(profileData);
+  const name = profileData.name;
+  const numOnPosts = profileData._count.posts;
+  const followers = profileData.followers.length;
+  const following = profileData.following.length;
+  const avatar = profileData.avatar;
+  const examplePhoto = "https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460__340.png"
+  //console.log(avatar)
+  const banner = document.querySelector("#profileBanner")
+  
+  //console.log(postData.author)
+  banner.innerHTML = ` 
+      
+      <div class="card col-12 bg-secondary mb-2" id="cardBanner">
+<div class="row g-0">
+  <div class="col-5">
+    <img
+
+      src="${avatar} "
+      alt="Profile photo"
+      class="img-fluid rounded-start"
+    />
+  </div>
+
+  <div class="col">
+    <div class="card-body">
+      <h5 class="card-title"><b>@${name}</b></h5>
+      <div class="col-md-8">
+      <i class="bi bi-sticky-fill text-primary"></i>
+        <p class="card-text d-inline text-muted"">${numOnPosts} posts</p>
+        </div>
+        <div class="col">
+        <i class="bi bi-file-person-fill text-primary"></i>
+        <p class="card-text d-inline text-muted">${following} following</p>
+        </div>
         
-        <div class="card col-12 bg-secondary mb-2" id="cardBanner">
-  <div class="row g-0">
-    <div class="col-5">
-      <img
-        src="${avatar}"
-        alt="Profile photo"
-        class="img-fluid rounded-start"
-      />
+        <div class="col">
+        <i class="bi bi-file-person text-primary"></i>
+        <p class="card-text d-inline text-muted"">${followers} followers</p>
+        </div>
+  
+        <div class="col-8 p-2 pt-4">
+        <button type="button" onclick="location.href='/profile'"  class="btn btn-dark col-10 me-2 d-block" id="goToProfile">Profile</button>
     </div>
+  </div>
+`;
+  }
 
-    <div class="col">
-      <div class="card-body">
-        <h5 class="card-title"><b>@${name}</b></h5>
-        <div class="col-md-8">
-        <i class="bi bi-sticky-fill text-primary"></i>
-          <p class="card-text d-inline text-muted"">${numOnPosts} posts</p>
-          </div>
-          <div class="col">
-          <i class="bi bi-file-person-fill text-primary"></i>
-          <p class="card-text d-inline text-muted">${following} following</p>
-          </div>
-          
-          <div class="col">
-          <i class="bi bi-file-person text-primary"></i>
-          <p class="card-text d-inline text-muted"">${followers} followers</p>
-          </div>
-    
-          <div class="col-8 p-2 pt-4">
-          <button type="button" onclick="location.href='/profile'"  class="btn btn-dark col-10 me-2 d-block" id="goToProfile">Profile</button>
-      </div>
-    </div>
-  `;
-    }
-
-   /**
-    * Function that display a banner of the logged in user
-    * @param {object} profileData 
-    */
-    
-    export function renderProfileTemplate(profileData){
-      bannerTemplate(profileData)
-    }
-
-   
+ /**
+  * Function that display a banner of the logged in user
+  * @param {object} profileData 
+  */
+  
+  export function renderProfileTemplate(profileData){
+    bannerTemplate(profileData)
+  }
 
 
         
